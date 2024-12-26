4d8246129f2937e040106d44198b59e06bdaf39f
bsarunmca <bsarunmca@gmail.com>
2024-12-26 13:35:24 +0530

    updating name logo

--- component/common/fullImageContent.jsx
+++ component/common/fullImageContent.jsx
@@ -14,9 +14,19 @@ function FullImageContent() {
                 <div className="imgOverLay" />
                 <div className="content">
                     <Badge variant="primary">
-                        <h5> Hello i 'm</h5>
+                        <h2 className="noMr"> Hello i 'm</h2>
                     </Badge>
-                    <h1> {constants.name} </h1>
+                    <div className="nameLogo">
+                        <div className="nameLogo-A">A</div>
+                        <div className="nameLogo-run">
+                            <div className="runner-logo">
+                                <Run />
+                            </div>
+                            <div className="run-txt">run</div>
+                        </div>
+                        <div className="nameLogo-BS">BS</div>
+                    </div>
+                    {/* <h1> {constants.name} </h1> */}
                     <h3> I build things for the web. </h3>
                     <p>
                         <FontAwesomeIcon icon={faEnvelope} />
